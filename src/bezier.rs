d40bcff513e76845ce060cf180a8a68a2c5e1c71
Andrei Monkin <monkin.andrey@gmail.com>
2024-07-11 06:37:36 +0300

    Fix bezier curves types issues

--- src/bezier.rs
+++ src/bezier.rs
@@ -1,23 +1,83 @@
-use crate::linear_speed::LinearSpeed;
 use crate::{Curve, CurvePoint, Distance};
 use num_traits::Float;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 
 /// Single point
 #[derive(Clone, PartialEq)]
-pub struct Bezier0<F: Float, P: CurvePoint<F>>(pub P);
+pub struct Bezier0<F: Float, P: CurvePoint<F>> {
+    pub point: P,
+    phantom_data: PhantomData<F>,
+}
+
+impl<F: Float, P: CurvePoint<F>> Bezier0<F, P> {
+    pub fn new(point: P) -> Self {
+        Self {
+            point,
+            phantom_data: PhantomData::default(),
+        }
+    }
+}
 
 /// Line
 #[derive(Clone, PartialEq)]
-pub struct Bezier1<F: Float, P: CurvePoint<F>>(pub P, pub P);
+pub struct Bezier1<F: Float, P: CurvePoint<F>> {
+    pub p0: P,
+    pub p1: P,
+    phantom_data: PhantomData<F>,
+}
+
+impl<F: Float, P: CurvePoint<F>> Bezier1<F, P> {
+    pub fn new(p0: P, p1: P) -> Self {
+        Self {
+            p0,
+            p1,
+            phantom_data: PhantomData::default(),
+        }
+    }
+}
 
 /// Quadratic bezier curve
 #[derive(Clone, PartialEq)]
-pub struct Bezier2<F: Float, P: CurvePoint<F>>(pub P, pub P, pub P);
+pub struct Bezier2<F: Float, P: CurvePoint<F>> {
+    pub p0: P,
+    pub p1: P,
+    pub p2: P,
+    phantom_data: PhantomData<F>,
+}
+
+impl<F: Float, P: CurvePoint<F>> Bezier2<F, P> {
+    pub fn new(p0: P, p1: P, p2: P) -> Self {
+        Self {
+            p0,
+            p1,
+            p2,
+            phantom_data: PhantomData::default(),
+        }
+    }
+}
 
 /// Cubic bezier curve
 #[derive(Clone, PartialEq)]
-pub struct Bezier3<F: Float, P: CurvePoint<F>>(pub P, pub P, pub P, pub P);
+pub struct Bezier3<F: Float, P: CurvePoint<F>> {
+    pub p0: P,
+    pub p1: P,
+    pub p2: P,
+    pub p3: P,
+    phantom_data: PhantomData<F>,
+}
+
+impl<F: Float, P: CurvePoint<F>> Bezier3<F, P> {
+    pub fn new(p0: P, p1: P, p2: P, p3: P) -> Self {
+        Self {
+            p0,
+            p1,
+            p2,
+            p3,
+            phantom_data: PhantomData::default(),
+        }
+    }
+}
 
 #[derive(Clone, PartialEq)]
 pub enum Bezier<F: Float, P: CurvePoint<F>> {
@@ -27,22 +87,22 @@ pub enum Bezier<F: Float, P: CurvePoint<F>> {
     C3(Bezier3<F, P>),
 }
 
-impl<F: Float, P: CurvePoint<F>> Bezier<F, P> {
-    fn content<'a>(&'a self) -> &'a dyn Curve<F, P> {
-        match self {
-            Bezier::C0(c) => c,
-            Bezier::C1(c) => c,
-            Bezier::C2(c) => c,
-            Bezier::C3(c) => c,
-        }
-    }
-}
-
 impl<F: Float, P: CurvePoint<F>> Copy for Bezier<F, P> where P: Copy {}
 impl<F: Float, P: CurvePoint<F>> Copy for Bezier0<F, P> where P: Copy {}
 impl<F: Float, P: CurvePoint<F>> Copy for Bezier1<F, P> where P: Copy {}
 impl<F: Float, P: CurvePoint<F>> Copy for Bezier2<F, P> where P: Copy {}
 impl<F: Float, P: CurvePoint<F>> Copy for Bezier3<F, P> where P: Copy {}
+
+macro_rules! for_every_level {
+    ($curve:ident, $name:ident, $block:block) => {
+        match $curve {
+            Bezier::C0($name) => $block,
+            Bezier::C1($name) => $block,
+            Bezier::C2($name) => $block,
+            Bezier::C3($name) => $block,
+        }
+    };
+}
 
 impl<F: Float, P: CurvePoint<F>> Debug for Bezier<F, P>
 where
@@ -50,12 +110,7 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_tuple("Bezier")
-            .field(match self {
-                Bezier::C0(c) => &c,
-                Bezier::C1(c) => &c,
-                Bezier::C2(c) => &c,
-                Bezier::C3(c) => &c,
-            })
+            .field(for_every_level!(self, c, { c }))
             .finish()
     }
 }
@@ -64,7 +119,7 @@ where
     P: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("Bezier0").field(&self.0).finish()
+        f.debug_tuple("Bezier0").field(&self.point).finish()
     }
 }
 impl<F: Float, P: CurvePoint<F>> Debug for Bezier1<F, P>
@@ -73,8 +128,8 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_tuple("Bezier1")
-            .field(&self.0)
-            .field(&self.1)
+            .field(&self.p0)
+            .field(&self.p1)
             .finish()
     }
 }
@@ -85,9 +140,9 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_tuple("Bezier2")
-            .field(&self.0)
-            .field(&self.1)
-            .field(&self.2)
+            .field(&self.p0)
+            .field(&self.p1)
+            .field(&self.p2)
             .finish()
     }
 }
@@ -97,30 +152,30 @@ where
     P: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Bezier3")
-            .field("0", &self.0)
-            .field("1", &self.1)
-            .field("2", &self.2)
-            .field("3", &self.3)
+        f.debug_tuple("Bezier3")
+            .field(&self.p0)
+            .field(&self.p1)
+            .field(&self.p2)
+            .field(&self.p3)
             .finish()
     }
 }
 
 impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier0<F, P> {
     fn value_at(&self, _t: F) -> P {
-        self.0.clone()
+        self.point.clone()
     }
 
     fn tangent_at(&self, _t: F) -> P {
-        P::zero()
+        self.point.scale(F::zero())
     }
 
     fn start_point(&self) -> P {
-        self.0.clone()
+        self.point.clone()
     }
 
     fn end_point(&self) -> P {
-        self.0.clone()
+        self.point.clone()
     }
 
     fn estimate_length(&self, _precision: F) -> F
@@ -133,26 +188,26 @@ impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier0<F, P> {
 
 impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier1<F, P> {
     fn value_at(&self, t: F) -> P {
-        self.0.add(&self.1.sub(&self.0).scale(&t))
+        self.p0.add(&self.p1.sub(&self.p0).scale(t))
     }
 
     fn tangent_at(&self, _t: F) -> P {
-        self.1.sub(&self.0)
+        self.p1.sub(&self.p0)
     }
 
     fn start_point(&self) -> P {
-        self.0.clone()
+        self.p0.clone()
     }
 
     fn end_point(&self) -> P {
-        self.1.clone()
+        self.p1.clone()
     }
 
     fn estimate_length(&self, _precision: F) -> F
     where
         P: Distance<F>,
     {
-        self.0.distance(&self.1)
+        self.p0.distance(&self.p1)
     }
 }
 
@@ -162,41 +217,48 @@ impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier2<F, P> {
         let t1 = F::one() - t;
         let t12 = t1 * t1;
 
-        self.0
-            .scale(&t12)
-            .add(&self.1.scale(&(F::from_f64(2.0).unwrap() * t1 * t)))
-            .add(&self.2.scale(&t2))
+        let two = F::one() + F::one();
+
+        self.p0
+            .scale(t12)
+            .add(&self.p1.scale(two * t1 * t))
+            .add(&self.p2.scale(t2))
     }
 
     fn tangent_at(&self, t: F) -> P {
-        let Bezier2(p1, p2, p3) = self;
+        let p0 = &self.p0;
+        let p1 = &self.p1;
+        let p2 = &self.p2;
+
         let two = F::one() + F::one();
 
         let t2 = t + t;
         let nt2 = two - t2;
 
-        let v1 = p2.sub(p1).scale(nt2);
-        let v2 = p3.sub(p2).scale(t2);
+        let v1 = p1.sub(p0).scale(nt2);
+        let v2 = p2.sub(p1).scale(t2);
 
         v1.add(&v2)
     }
 
     fn start_point(&self) -> P {
-        self.0.clone()
+        self.p0.clone()
     }
 
     fn end_point(&self) -> P {
-        self.2.clone()
+        self.p2.clone()
     }
 
     fn estimate_length(&self, precision: F) -> F
     where
         P: Distance<F>,
     {
-        let Bezier2(p1, p2, p3) = self;
+        let p0 = &self.p0;
+        let p1 = &self.p1;
+        let p2 = &self.p2;
 
-        let min = p1.distance(p3);
-        let max = p1.distance(p2) + p2.distance(p3);
+        let min = p0.distance(p1);
+        let max = p0.distance(p1) + p1.distance(p2);
 
         let half = F::one() / (F::one() + F::one());
 
@@ -205,12 +267,12 @@ impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier2<F, P> {
         } else if (max - min) / max < precision {
             (min + max) * half
         } else {
-            let m01 = p1.add(&p2).scale(half);
-            let m12 = p2.add(&p3).scale(half);
+            let m01 = p0.add(&p1).scale(half);
+            let m12 = p1.add(&p2).scale(half);
             let m = m01.add(&m12).scale(half);
 
-            let b1 = Bezier2(p1, m01, m.clone());
-            let b2 = Bezier2(m, m12, p3);
+            let b1 = Bezier2::new(p0.clone(), m01, m.clone());
+            let b2 = Bezier2::new(m, m12, p2.clone());
 
             b1.estimate_length(precision) + b2.estimate_length(precision)
         }
@@ -228,14 +290,17 @@ impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier3<F, P> {
         let nt2 = nt * nt;
         let nt3 = nt2 * nt;
 
-        self.0
-            .scale(&nt3)
-            .add(&self.1.scale(&(three * nt2 * t)))
-            .add(&self.2.scale(&(three * nt * t2)).add(&self.3.scale(&t3)))
+        self.p0
+            .scale(nt3)
+            .add(&self.p1.scale(three * nt2 * t))
+            .add(&self.p2.scale(three * nt * t2).add(&self.p3.scale(t3)))
     }
 
     fn tangent_at(&self, t: F) -> P {
-        let Bezier3(p0, p1, p2, p3) = self;
+        let p0 = &self.p0;
+        let p1 = &self.p1;
+        let p2 = &self.p2;
+        let p3 = &self.p3;
 
         let three = F::one() + F::one() + F::one();
         let six = three + three;
@@ -245,46 +310,49 @@ impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier3<F, P> {
         let nt = F::one() - t;
         let nt2 = nt * nt;
 
-        let v1 = p1.sub(&p0).scale(&(three * nt2));
-        let v2 = p2.sub(&p1).scale(&(six * nt * t));
-        let v3 = p3.sub(&p2).scale(&(three * t2));
+        let v1 = p1.sub(&p0).scale(three * nt2);
+        let v2 = p2.sub(&p1).scale(six * nt * t);
+        let v3 = p3.sub(&p2).scale(three * t2);
 
         v1.add(&v2).add(&v3)
     }
 
     fn start_point(&self) -> P {
-        self.0.clone()
+        self.p0.clone()
     }
 
     fn end_point(&self) -> P {
-        self.3.clone()
+        self.p3.clone()
     }
 
     fn estimate_length(&self, precision: F) -> F
     where
         P: Distance<F>,
     {
-        let Bezier3(p1, p2, p3, p4) = self;
+        let p0 = &self.p0;
+        let p1 = &self.p1;
+        let p2 = &self.p2;
+        let p3 = &self.p3;
 
-        let min = p1.distance(p4);
-        let max = p1.distance(p2) + p2.distance(p3) + p3.distance(p4);
+        let min = p0.distance(p3);
+        let max = p0.distance(p1) + p1.distance(p2) + p2.distance(p3);
 
         let half = F::one() / (F::one() + F::one());
 
         if max == F::zero() {
             F::zero()
         } else if (max - min) / max < precision {
-            (min + max) * half;
+            (min + max) * half
         } else {
-            let m01 = p1.add(&p2).scale(half);
-            let m12 = p2.add(&p3).scale(half);
-            let m23 = p3.add(&p4).scale(half);
+            let m01 = p0.add(&p1).scale(half);
+            let m12 = p1.add(&p2).scale(half);
+            let m23 = p2.add(&p3).scale(half);
             let m012 = m01.add(&m12).scale(half);
             let m123 = m12.add(&m23).scale(half);
             let m = m012.add(&m123).scale(half);
 
-            let b1 = Bezier3(p1, m01, m012, m.clone());
-            let b2 = Bezier3(m, m123, m23, p4);
+            let b1 = Bezier3::new(p0.clone(), m01, m012, m.clone());
+            let b2 = Bezier3::new(m, m123, m23, p3.clone());
 
             b1.estimate_length(precision) + b2.estimate_length(precision)
         }
@@ -293,25 +361,25 @@ impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier3<F, P> {
 
 impl<F: Float, P: CurvePoint<F>> Curve<F, P> for Bezier<F, P> {
     fn value_at(&self, t: F) -> P {
-        self.content().value_at(t)
+        for_every_level!(self, c, { c.value_at(t) })
     }
 
     fn tangent_at(&self, t: F) -> P {
-        self.content().tangent_at(t)
+        for_every_level!(self, c, { c.tangent_at(t) })
     }
 
     fn start_point(&self) -> P {
-        self.content().start_point()
+        for_every_level!(self, c, { c.start_point() })
     }
 
     fn end_point(&self) -> P {
-        self.content().end_point()
+        for_every_level!(self, c, { c.end_point() })
     }
 
     fn estimate_length(&self, precision: F) -> F
     where
         P: Distance<F>,
     {
-        self.content().estimate_length(precision)
+        for_every_level!(self, c, { c.estimate_length(precision) })
     }
 }
