aa0236bb00e44967cbdb06ab6d16520ec9e2bcc3
Andrei Monkin <monkin.andrey@gmail.com>
2024-07-11 18:00:40 +0300

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 mod bezier;
 mod composed_curve;
 mod curve;
+mod curve_iterator;
 mod curve_point;
 mod distance;
-mod smooth_array;
-mod curve_iterator;
 mod linear_speed;
+mod smooth_array;
 
 pub use bezier::{Bezier0, Bezier1, Bezier2, Bezier3};
+pub use composed_curve::ComposedCurve;
 pub use curve::Curve;
 pub use curve_point::CurvePoint;
 pub use distance::Distance;
-pub use composed_curve::ComposedCurve;
 pub use linear_speed::LinearSpeed;
